e2d15803c4df046ce99c76abd6da58b8083a08fc
Dhanalakshmi Narala <dhanalakshmi.narala@mable.com.au>
2024-10-18 12:22:42 +0530

    Add product of two using sum functionality

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -2,6 +2,33 @@ export const sum = (nums: number[]): number => {
   return nums.reduce((result, current) => result + current, 0);
 };
 
+export const productUsingSum = (nums: number[]): number => {
+  if (nums.length == 0) {
+    return 0;
+  }
+
+  if (nums.length == 1) {
+    return nums[0];
+  }
+
+  let result = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    result += productOfTwo(result, nums[i + 1]);
+  }
+  return result;
+};
+
+export const productOfTwo = (numOne: number, numTwo: number): number => {
+  const min = Math.min(numOne, numTwo);
+  const other = numOne == min ? numTwo : numOne;
+
+  let result = 0;
+  for (let i = 0; i < min; i++) {
+    result += other;
+  }
+  return result;
+};
+
 export const getNegativeNumbers = (nums: number[]): number[] => {
   return nums.filter((num) => num < 0);
 };
--- tests/utils/math.test.ts
+++ tests/utils/math.test.ts
@@ -1,33 +1,49 @@
 import {
   getNegativeNumbers,
   getNumbersInRange,
+  productOfTwo,
+  productUsingSum,
   sum,
 } from '../../src/utils/math';
 
-describe('sum', () => {
-  it('should return sum of numbers array', () => {
-    const result = sum([1, 2, 3, 5]);
-    expect(result).toBe(11);
+describe('math', () => {
+  describe('sum', () => {
+    it('should return sum of numbers array', () => {
+      const result = sum([1, 2, 3, 5]);
+      expect(result).toBe(11);
+    });
+
+    it('should return sum of floating numbers array', () => {
+      const result = sum([1, 2.5, 3, 0.98]);
+      expect(result).toBe(7.48);
+    });
+
+    it('should return negative numbers', () => {
+      const resultOne = getNegativeNumbers([1, -5, 3, -10]);
+      expect(resultOne).toEqual([-5, -10]);
+
+      const resultTwo = getNegativeNumbers([-1111, -500, -3, 0]);
+      expect(resultTwo).toEqual([-1111, -500, -3]);
+    });
+
+    it('should return numbers which are less than or equal to 1000', () => {
+      const resultOne = getNumbersInRange([1, -5, 3000, 1000], 1000);
+      expect(resultOne).toEqual([1, -5, 1000]);
+
+      const resultTwo = getNumbersInRange([999, 100, 3000, 4000], 1000);
+      expect(resultTwo).toEqual([999, 100]);
+    });
   });
 
-  it('should return sum of floating numbers array', () => {
-    const result = sum([1, 2.5, 3, 0.98]);
-    expect(result).toBe(7.48);
-  });
+  describe('productOfTwo', () => {
+    it('should give 10 for 2*5', () => {
+      const product = productOfTwo(2, 5);
+      expect(product).toBe(10);
+    });
 
-  it('should return negative numbers', () => {
-    const resultOne = getNegativeNumbers([1, -5, 3, -10]);
-    expect(resultOne).toEqual([-5, -10]);
-
-    const resultTwo = getNegativeNumbers([-1111, -500, -3, 0]);
-    expect(resultTwo).toEqual([-1111, -500, -3]);
-  });
-
-  it('should return numbers which are less than or equal to 1000', () => {
-    const resultOne = getNumbersInRange([1, -5, 3000, 1000], 1000);
-    expect(resultOne).toEqual([1, -5, 1000]);
-
-    const resultTwo = getNumbersInRange([999, 100, 3000, 4000], 1000);
-    expect(resultTwo).toEqual([999, 100]);
+    it('should give 1000 for 1*1000', () => {
+      const product = productOfTwo(1, 1000);
+      expect(product).toBe(1000);
+    });
   });
 });
